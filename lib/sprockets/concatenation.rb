92afccd42184680d2190b92a842ff03118b1ff5c
Joshua Peek <josh@joshpeek.com>
2009-10-31 11:35:24 -0500

    Add MD5 to Concatenation

--- lib/sprockets/concatenation.rb
+++ lib/sprockets/concatenation.rb
@@ -1,26 +1,34 @@
+require 'digest/md5'
+
 module Sprockets
   class Concatenation
     attr_reader :source_lines
-    
+
     def initialize
       @source_lines = []
       @source_file_mtimes = {}
+      @source_md5 = nil
     end
-    
+
     def record(source_line)
+      @source_md5 = nil
       source_lines << source_line
       record_mtime_for(source_line.source_file)
       source_line
     end
-    
+
     def to_s
       source_lines.join
+    end
+
+    def md5
+      @source_md5 ||= Digest::MD5.hexdigest(to_s)
     end
 
     def mtime
       @source_file_mtimes.values.max
     end
-    
+
     def save_to(filename)
       timestamp = mtime
       File.open(filename, "w") { |file| file.write(to_s) }
