3c15eb29870f1da27a0b8d991e44131196d29340
Joshua Peek <josh@joshpeek.com>
2010-11-12 22:29:21 -0600

    Add initial server test

---
+++ test/test_server.rb
@@ -0,0 +1,36 @@
+require "sprockets_test"
+
+require 'rack/builder'
+require 'rack/test'
+
+class TestServer < Sprockets::TestCase
+  include Rack::Test::Methods
+
+  def setup
+    @env = Sprockets::Environment.new
+    @env.paths << fixture_path("server/app/javascripts")
+  end
+
+  def javascripts_app
+    Sprockets::Server.new(@env)
+  end
+
+  def default_app
+    javascripts_app = self.javascripts_app
+
+    Rack::Builder.new do
+      map "/javascripts" do
+        run javascripts_app
+      end
+    end
+  end
+
+  def app
+    @app ||= default_app
+  end
+
+  test "serve single source file" do
+    get "/javascripts/foo.js"
+    assert_equal "var foo;\n", last_response.body
+  end
+end
