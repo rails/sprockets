b29d8f613616d596a578d3bc00132a65b3b6fc76
Sam Stephenson <sam@37signals.com>
2010-11-23 10:07:31 -0600

    Stub asset tests

---
+++ test/test_asset.rb
@@ -0,0 +1,48 @@
+require "sprockets_test"
+
+class AssetTest < Sprockets::TestCase
+  def setup
+    @env = Sprockets::Environment.new
+    @env.paths << fixture_path('asset')
+    @asset = Sprockets::Asset.new(@env, "application/javascript", ".js")
+  end
+
+  test "requiring the same file multiple times has no effect" do
+    @asset.require(source_file("project.js"))
+    source = @asset.source
+
+    @asset.require(source_file("project.js"))
+    assert_equal source, @asset.source
+  end
+
+  test "requiring a file of a different format raises an exception" do
+    assert_raise Sprockets::ContentTypeMismatch do
+      @asset.require(source_file("project.css"))
+    end
+  end
+
+  test "dependencies appear in the source before files that required them" do
+  end
+
+  test "processing a source file with no engine extensions" do
+  end
+
+  test "processing a source file with one engine extension" do
+  end
+
+  test "processing a source file with multiple engine extensions" do
+  end
+
+  test "included dependencies are inserted after the header of the dependent file" do
+  end
+
+  test "asset mtime is the latest mtime of all processed sources" do
+  end
+
+  test "asset inherits the format extension and content type of the original file" do
+  end
+
+  def source_file(logical_path)
+    @env.find_source_file(logical_path)
+  end
+end
