366a2836ebe7faa0eacbfef3a3c15bc0ef5271e8
Joshua Peek <josh@joshpeek.com>
2011-03-03 10:36:56 -0600

    Add Rails helper

---
+++ lib/sprockets/rails.rb
@@ -0,0 +1,29 @@
+require 'sprockets'
+
+module Rails
+  def self.default_sprockets_paths
+    ["app/assets",
+     "app/javascripts",
+     "vendor/sprockets/*/src",
+     "vendor/sprockets/*/lib",
+     "vendor/plugins/*/assets",
+     "vendor/plugins/*/javascripts",
+     "vendor/plugins/*/app/assets"
+     "vendor/plugins/*/app/javascripts"]
+  end
+
+  def self.assets
+    @assets ||= begin
+      env = Sprockets::Environment.new(self.root.to_s)
+      env.logger = self.logger
+
+      self.default_sprockets_paths.each do |pattern|
+        Dir[pattern].each do |dir|
+          env.paths << dir
+        end
+      end
+
+      env
+    end
+  end
+end
