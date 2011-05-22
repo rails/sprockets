fe567986e4147ad8564caefe288d4801729edc02
Joshua Peek <josh@joshpeek.com>
2011-05-22 15:29:18 -0500

    Blacklist tilt 1.3.0 in gemspec

--- lib/sprockets/notifier.rb
+++ lib/sprockets/notifier.rb
@@ -1,5 +1,4 @@
 require 'logger'
-require 'tilt'
 
 module Sprockets
   module Notifier
@@ -12,10 +11,6 @@ module Sprockets
     def self.notify(logger = stderr)
       if defined?(Rails) && Rails::VERSION::STRING.match(/^3\.1\.0\.beta/)
         logger.warn "WARNING: Sprockets #{Sprockets::VERSION} is incompatible with Rails #{Rails::VERSION::STRING}. Please upgrade to Rails 3.1.0.rc1 or higher."
-      end
-
-      if Tilt::VERSION == '1.3'
-        logger.warn "WARNING: Sprockets #{Sprockets::VERSION} is incompatible with Tilt 1.3. Please upgrade to Tilt 1.3.1 or higher."
       end
     end
   end
