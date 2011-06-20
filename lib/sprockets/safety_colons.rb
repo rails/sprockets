9e7db1ff143467d2599bdb8a188131c664bad85c
Joshua Peek <josh@joshpeek.com>
2011-06-19 19:33:46 -0500

    Doc semicolons

--- lib/sprockets/safety_colons.rb
+++ lib/sprockets/safety_colons.rb
@@ -1,14 +1,21 @@
 require 'tilt'
 
 module Sprockets
+  # For JS developers who are colonfobic, concatenating JS files using
+  # the module pattern usually leads to syntax errors.
+  #
+  # The `SafetyColons` processor will insert missing semicolons to the
+  # end of the file.
   class SafetyColons < Tilt::Template
     def prepare
     end
 
     def evaluate(context, locals, &block)
+      # If the file is blank or ends in a semicolon, leave it as is
       if data =~ /\A\s*\Z/m || data =~ /;\s*\Z/m
         data
       else
+        # Otherwise, append a semicolon and newline
         "#{data};\n"
       end
     end
