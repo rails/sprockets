b0787e160750dd64a0cc14dfc09817764f151912
Joshua Peek <josh@joshpeek.com>
2011-06-19 22:44:17 -0500

    Doc Base

--- lib/sprockets/base.rb
+++ lib/sprockets/base.rb
@@ -10,39 +10,71 @@ require 'sprockets/trail'
 require 'pathname'
 
 module Sprockets
+  # `Base` class for `Environment` and `Index`.
   class Base
     include Digest
     include Caching, Processing, Server, StaticCompilation, Trail
 
+    # Get and set `Logger` instance.
     attr_accessor :logger
 
+    # Get `Context` class.
+    #
+    # This class maybe mutated and mixed in with custom helpers.
+    #
+    #     environment.context_class.instance_eval do
+    #       include MyHelpers
+    #       def asset_url; end
+    #     end
+    #
     attr_reader :context_class
 
+    # Get persistent cache store
     attr_reader :cache
 
+    # Set persistent cache store
+    #
+    # The cache store must implement a pair of getters and
+    # setters. Either `get(key)`/`set(key, value)`,
+    # `[key]`/`[key]=value`, `read(key)`/`write(key, value)`.
     def cache=(cache)
       expire_index!
       @cache = cache
     end
 
+    # Return an `Index`. Must be implemented by the subclass.
     def index
       raise NotImplementedError
     end
 
+    # Works like `Dir.entries`.
+    #
+    # Subclasses may cache this method.
     def entries(pathname)
       trail.entries(pathname)
     end
 
+    # Works like `File.stat`.
+    #
+    # Subclasses may cache this method.
     def stat(path)
       trail.stat(path)
     end
 
+    # Read and compute digest of filename.
+    #
+    # Subclasses may cache this method.
     def file_digest(path, data = nil)
       if stat = self.stat(path)
+        # `data` maybe provided
         if data
           digest.update(data)
+
+        # If its a file, digest the contents
         elsif stat.file?
           digest.file(path)
+
+        # If its a directive, digest the list of filenames
         elsif stat.directory?
           contents = self.entries(path).join(',')
           digest.update(contents)
@@ -50,14 +82,17 @@ module Sprockets
       end
     end
 
+    # Internal. Return a `AssetAttributes` for `path`.
     def attributes_for(path)
       AssetAttributes.new(self, path)
     end
 
+    # Internal. Return content type of `path`.
     def content_type_of(path)
       attributes_for(path).content_type
     end
 
+    # Find asset by logical path or expanded path.
     def find_asset(path, options = {})
       pathname = Pathname.new(path)
 
@@ -69,25 +104,37 @@ module Sprockets
       end
     end
 
+    # Preferred `find_asset` shorthand.
+    #
+    #     environment['application.js']
+    #
     def [](*args)
       find_asset(*args)
     end
 
-    def build_asset(logical_path, pathname, options)
-      pathname = Pathname.new(pathname)
-
-      if attributes_for(pathname).processors.any?
-        BundledAsset.new(self, logical_path, pathname, options)
-      else
-        StaticAsset.new(self, logical_path, pathname)
-      end
-    end
-
     protected
+      # Clear index after mutating state. Must be implemented by the subclass.
       def expire_index!
         raise NotImplementedError
       end
 
+      def build_asset(logical_path, pathname, options)
+        pathname = Pathname.new(pathname)
+
+        # If there are any processors to run on the pathname, use
+        # `BundledAsset`. Otherwise use `StaticAsset` and treat is as binary.
+        if attributes_for(pathname).processors.any?
+          BundledAsset.new(self, logical_path, pathname, options)
+        else
+          StaticAsset.new(self, logical_path, pathname)
+        end
+      end
+
+      # Reverse guess logical path for fully expanded path.#
+      #
+      # This has some known issues. For an example if a file is
+      # shaddowed in the path, but is required relatively, its logical
+      # path will be incorrect.
       def detect_logical_path(filename)
         if root_path = paths.detect { |path| filename.to_s[path] }
           root_pathname = Pathname.new(root_path)
