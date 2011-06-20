0b062564776c68daa54d65c7f6138f3e63f33fd2
Joshua Peek <josh@joshpeek.com>
2011-06-19 19:27:05 -0500

    Doc Eco and Ejs Tilt engines

--- lib/sprockets/eco_template.rb
+++ lib/sprockets/eco_template.rb
@@ -1,11 +1,23 @@
 require 'tilt'
 
 module Sprockets
+  # Tilt engine class for the Eco compiler. Depends on the `eco` gem.
+  #
+  # For more infomation see:
+  #
+  #   https://github.com/sstephenson/ruby-eco
+  #   https://github.com/sstephenson/eco
+  #
   class EcoTemplate < Tilt::Template
+    # Eco templates always produced compiled JS. We can set Tilt's
+    # default mime type hint.
     def self.default_mime_type
       'application/javascript'
     end
 
+    # Autoload eco library. If the library isn't loaded, Tilt will produce
+    # a thread safetly warning. If you intend to use `.eco` files, you
+    # should explicitly require it.
     def initialize_engine
       require_template_library 'eco'
     end
@@ -13,6 +25,13 @@ module Sprockets
     def prepare
     end
 
+    # Compile template data with Eco compiler.
+    #
+    # Returns a JS function definition String. The result should be
+    # assigned to a JS variable.
+    #
+    #     # => "function(...) {...}"
+    #
     def evaluate(scope, locals, &block)
       Eco.compile(data)
     end
--- lib/sprockets/ejs_template.rb
+++ lib/sprockets/ejs_template.rb
@@ -1,11 +1,22 @@
 require 'tilt'
 
 module Sprockets
+  # Tilt engine class for the EJS compiler. Depends on the `ejs` gem.
+  #
+  # For more infomation see:
+  #
+  #   https://github.com/sstephenson/ruby-ejs
+  #
   class EjsTemplate < Tilt::Template
+    # EJS templates always produced compiled JS. We can set Tilt's
+    # default mime type hint.
     def self.default_mime_type
       'application/javascript'
     end
 
+    # Autoload ejs library. If the library isn't loaded, Tilt will produce
+    # a thread safetly warning. If you intend to use `.ejs` files, you
+    # should explicitly require it.
     def initialize_engine
       require_template_library 'ejs'
     end
@@ -13,6 +24,13 @@ module Sprockets
     def prepare
     end
 
+    # Compile template data with EJS compiler.
+    #
+    # Returns a JS function definition String. The result should be
+    # assigned to a JS variable.
+    #
+    #     # => "function(obj){...}"
+    #
     def evaluate(scope, locals, &block)
       EJS.compile(data)
     end
