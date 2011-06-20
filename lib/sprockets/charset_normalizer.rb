f2c62249870a84b8113f197cdb3a519705902723
Joshua Peek <josh@joshpeek.com>
2011-06-19 19:27:13 -0500

    Doc charset normalizer

--- lib/sprockets/charset_normalizer.rb
+++ lib/sprockets/charset_normalizer.rb
@@ -1,6 +1,18 @@
 require 'tilt'
 
 module Sprockets
+  # Some browsers have issues with stylesheets that contain multiple
+  # `@charset` definitions. The issue surfaces while using Sass since
+  # it inserts a `@charset` at the top of each file. Then Sprockets
+  # concatenates them together.
+  #
+  # The `CharsetNormalizer` processor strips out multiple `@charset`
+  # definitions.
+  #
+  # The current implementation is naive. It picks the first `@charset`
+  # it sees and strips the others. This works for most people because
+  # the other definitions are usually `UTF-8`. A more sophisticated
+  # approach would be to re-encode stylesheets with mixed encodings.
   class CharsetNormalizer < Tilt::Template
     def prepare
     end
@@ -8,11 +20,13 @@ module Sprockets
     def evaluate(context, locals, &block)
       charset = nil
 
+      # Find and strip out any `@charset` definitions
       filtered_data = data.gsub(/^@charset "([^"]+)";$/) {
         charset ||= $1; ""
       }
 
       if charset
+        # If there was a charset, move it to the top
         "@charset \"#{charset}\";#{filtered_data}"
       else
         data
