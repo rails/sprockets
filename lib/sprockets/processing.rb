48b85f7162fcfa363a6649d6be976cd6febbe088
Joshua Peek <josh@joshpeek.com>
2011-08-04 18:45:39 -0500

    to_s symbol for 1.8

--- lib/sprockets/processing.rb
+++ lib/sprockets/processing.rb
@@ -268,11 +268,11 @@ module Sprockets
 
     private
       def add_engine_to_trail(ext, klass)
-        @trail.append_extension(ext)
+        @trail.append_extension(ext.to_s)
 
         if klass.respond_to?(:default_mime_type) && klass.default_mime_type
           if format_ext = extension_for_mime_type(klass.default_mime_type)
-            @trail.alias_extension(ext, format_ext)
+            @trail.alias_extension(ext.to_s, format_ext)
           end
         end
       end
