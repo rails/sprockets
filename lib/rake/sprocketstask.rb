590e8b1a247ba3a750876e2a884270a06a6d6fc8
Joshua Peek <josh@joshpeek.com>
2011-12-13 13:24:33 -0600

    Improve rake task logging

--- lib/rake/sprocketstask.rb
+++ lib/rake/sprocketstask.rb
@@ -16,17 +16,55 @@ module Rake
 
     attr_accessor :logger
 
+    def log_level
+      @logger.level
+    end
+
+    def log_level=(level)
+      if level.is_a?(Integer)
+        @logger.level = level
+      else
+        @logger.level = Logger.const_get(level.to_s.upcase)
+      end
+    end
+
     def initialize(name = :bundle)
       init(name)
       yield self if block_given?
-      @environment.logger       = Logger.new($stderr)
-      @environment.logger.level = Logger::INFO
+      @environment = @environment.index
       define
     end
 
     def init(name)
-      @name        = name
-      @environment = Sprockets::Environment.new(Dir.pwd)
+      @name         = name
+      @environment  = Sprockets::Environment.new(Dir.pwd)
+      @logger       = Logger.new($stderr)
+      @logger.level = Logger::WARN
+    end
+
+    def with_logger
+      old_logger = @environment.logger
+      @environment.logger = @logger
+      yield
+    ensure
+      @environment.logger = old_logger
+    end
+
+    def benchmark
+      start_time = Time.now.to_f
+      yield
+      elapsed_time = ((Time.now.to_f - start_time) * 1000).to_i
+    end
+
+    def compile(logical_path)
+      asset = nil
+      with_logger do
+        ms = benchmark do
+          asset = environment.find_asset(logical_path)
+        end
+        environment.logger.warn "Compiled #{logical_path}  (#{ms}ms)"
+      end
+      asset
     end
 
     def define
@@ -34,9 +72,14 @@ module Rake
 
       bundles.each do |logical_path|
         task "#{name}:#{logical_path}" => bundle_dir do
-          if asset = environment.find_asset(logical_path)
+          if asset = compile(logical_path)
             target = File.join(bundle_dir, asset.digest_path)
-            asset.write_to target
+            if File.exist?(target)
+              logger.debug "Skipping #{target}, already exists"
+            else
+              logger.info "Writing #{target}"
+              asset.write_to target
+            end
           end
         end
       end
@@ -56,7 +99,7 @@ module Rake
         files = Dir["#{bundle_dir}/*"]
 
         bundles.each do |logical_path|
-          if asset = environment.find_asset(logical_path)
+          if asset = compile(logical_path)
             target = File.join(bundle_dir, asset.digest_path)
             files.delete target
           end
