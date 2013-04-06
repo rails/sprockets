04a059c164a5cb2b4de183ab8054ec3c0146f3b0
Joshua Peek <josh@joshpeek.com>
2013-04-06 13:34:36 -0500

    Support uglifier 2.x option style

--- lib/sprockets/uglifier_compressor.rb
+++ lib/sprockets/uglifier_compressor.rb
@@ -16,7 +16,14 @@ module Sprockets
     end
 
     def evaluate(context, locals, &block)
-      Uglifier.new(:copyright => false).compile(data)
+      # Feature detect Uglifier 2.0 option support
+      if Uglifier::DEFAULTS[:copyright]
+        # Uglifier < 2.x
+        Uglifier.new(:copyright => false).compile(data)
+      else
+        # Uglifier >= 2.x
+        Uglifier.new(:comments => :none).compile(data)
+      end
     end
   end
 end
