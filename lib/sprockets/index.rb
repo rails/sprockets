9f18828cee1d41acdf8bb4de0dd3a02a32ef843f
Joshua Peek <josh@joshpeek.com>
2014-02-26 15:27:26 -0600

    Move cache_asset closer to call site

--- lib/sprockets/index.rb
+++ lib/sprockets/index.rb
@@ -97,5 +97,38 @@ module Sprockets
           end
         end
       end
+
+      # Cache helper method. Takes a `path` argument which maybe a
+      # logical path or fully expanded path. The `&block` is passed
+      # for finding and building the asset if its not in cache.
+      def cache_asset(path)
+        path_cache_key = "asset/#{path.to_s.sub(root, '')}"
+
+        # If `cache` is not set, return fast
+        if cache.nil?
+          yield
+
+        # Check cache for `path`
+        elsif (asset = Asset.from_hash(self, cache_adapter.get(path_cache_key))) && asset.fresh?(self)
+          asset
+
+         # Otherwise yield block that slowly finds and builds the asset
+        elsif asset = yield
+          hash = {}
+          asset.encode_with(hash)
+
+          # Save the asset to its path
+          cache_adapter.set(path_cache_key, hash)
+
+          # Since path maybe a logical or full pathname, save the
+          # asset its its full path too
+          if path.to_s != asset.pathname.to_s
+            pathname_cache_key = "asset/#{asset.pathname.to_s.sub(root, '')}"
+            cache_adapter.set(pathname_cache_key, hash)
+          end
+
+          asset
+        end
+      end
   end
 end
