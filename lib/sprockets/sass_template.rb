4dd869b38ab66744a979c9548e54e2900794791b
Joshua Peek <josh@joshpeek.com>
2014-03-12 14:18:33 -0500

    Avoid exposing a context reference to SassImporter
    Make SassImporter safely marshalable

--- lib/sprockets/sass_template.rb
+++ lib/sprockets/sass_template.rb
@@ -42,15 +42,21 @@ module Sprockets
         :line => line,
         :syntax => syntax,
         :cache_store => cache_store,
-        :importer => SassImporter.new(context, context.pathname),
-        :load_paths => context.environment.paths.map { |path| SassImporter.new(context, path) },
+        :importer => SassImporter.new(context.pathname.to_s),
+        :load_paths => context.environment.paths.map { |path| SassImporter.new(path.to_s) },
         :sprockets => {
           :context => context,
           :environment => context.environment
         }
       }
 
-      ::Sass::Engine.new(data, options).render
+      result = ::Sass::Engine.new(data, options).render
+
+      # Track all imported files
+      filenames = ([options[:importer].imported_filenames] + options[:load_paths].map(&:imported_filenames)).flatten.uniq
+      filenames.each { |filename| context.depend_on(filename) }
+
+      result
     rescue ::Sass::SyntaxError => e
       # Annotates exception message with parse line number
       context.__LINE__ = e.sass_backtrace.first[:line]
