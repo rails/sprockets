3c4143e0bdd75636ab004f251563e9aed5d126f6
Joshua Peek <josh@joshpeek.com>
2014-03-16 20:29:51 -0400

    Use call api for less template

--- lib/sprockets/less_template.rb
+++ lib/sprockets/less_template.rb
@@ -1,13 +1,13 @@
 module Sprockets
-  class LessTemplate < Template
-    def render(context)
+  module LessTemplate
+    def self.call(input)
       require 'less' unless defined? ::Less
 
       if ::Less.const_defined? :Engine
-        engine = ::Less::Engine.new(data)
+        engine = ::Less::Engine.new(input[:data])
       else
-        parser = ::Less::Parser.new(:filename => context.pathname.to_s)
-        engine = parser.parse(data)
+        parser = ::Less::Parser.new(:filename => input[:filename])
+        engine = parser.parse(input[:data])
       end
 
       engine.to_css
