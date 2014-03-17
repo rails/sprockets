d5c4e82fc9fed4af7b8a382ed669aad87f01bd2c
Joshua Peek <josh@joshpeek.com>
2014-03-16 20:31:47 -0400

    Use call api for erb template

--- lib/sprockets/erb_template.rb
+++ lib/sprockets/erb_template.rb
@@ -1,11 +1,12 @@
 module Sprockets
-  class ERBTemplate < Template
-    def render(context)
+  module ERBTemplate
+    def self.call(input)
       require 'erb' unless defined? ::ERB
-      engine = ::ERB.new(data, nil, '<>')
+      engine = ::ERB.new(input[:data], nil, '<>')
       method_name = "__sprockets_#{Thread.current.object_id.abs}"
+      context = input[:context]
       klass = (class << context; self; end)
-      engine.def_method(klass, method_name, context.pathname.to_s)
+      engine.def_method(klass, method_name, input[:filename])
       context.send(method_name)
     end
   end
