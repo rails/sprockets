fdb540d8b2cf149ac1ceeb25da225daf07de7d07
Joshua Peek <josh@joshpeek.com>
2014-03-23 15:54:44 +0000

    Doc closure compiler

--- lib/sprockets/closure_compressor.rb
+++ lib/sprockets/closure_compressor.rb
@@ -1,6 +1,18 @@
 require 'closure-compiler'
 
 module Sprockets
+  # Public: Closure Compiler minifier.
+  #
+  # To accept the default options
+  #
+  #     environment.register_bundle_processor 'text/js',
+  #       Sprockets::ClosureCompressor
+  #
+  # Or to pass options to the Closure::Compiler class.
+  #
+  #     environment.register_bundle_processor 'text/js',
+  #       Sprockets::ClosureCompressor.new({ ... })
+  #
   class ClosureCompressor
     def self.call(*args)
       new.call(*args)
