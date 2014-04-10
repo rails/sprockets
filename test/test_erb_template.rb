763d2a956d61705b63a0aba3608392d38bdafeb1
Joshua Peek <josh@joshpeek.com>
2014-04-09 23:21:47 -0500

    Test erb depend_on

--- test/test_erb_template.rb
+++ test/test_erb_template.rb
@@ -16,4 +16,23 @@ class TestERBTemplate < Sprockets::TestCase
     output = "var data = {\"foo\":true};"
     assert_equal output, Sprockets::ERBTemplate.call(input)[:data]
   end
+
+  test "compile erb template with depend_on call" do
+    environment = Sprockets::Environment.new(FIXTURE_ROOT)
+    environment.append_path fixture_path('default')
+
+    path = fixture_path('default/gallery.js')
+    input = {
+      environment: environment,
+      filename: "foo.js.erb",
+      content_type: 'application/javascript',
+      data: "<%= depend_on('#{path}') %>\nvar data = 'DATA';",
+      cache: Sprockets::Cache.new
+    }
+
+    output = "var data = 'DATA';"
+    result = Sprockets::ERBTemplate.call(input)
+    assert_equal output, result[:data]
+    assert_equal path, result[:dependency_paths].first
+  end
 end
