20c384e2d19d5480871096e9fd769618bb44af6d
Joshua Peek <josh@joshpeek.com>
2014-05-31 00:49:31 -0500

    Doc decode_unicode_bom

--- lib/sprockets/encoding.rb
+++ lib/sprockets/encoding.rb
@@ -2,6 +2,7 @@ module Sprockets
   module Encoding
     extend self
 
+    # Internal: Mapping unicode encodings to byte order markers.
     BOM = {
       ::Encoding::UTF_32LE => [0xFF, 0xFE, 0x00, 0x00],
       ::Encoding::UTF_32BE => [0x00, 0x00, 0xFE, 0xFF],
@@ -10,6 +11,12 @@ module Sprockets
       ::Encoding::UTF_16BE => [0xFE, 0xFF]
     }
 
+    # Public: Decode and strip BOM from possible unicode string.
+    #
+    # str - ASCII-8BIT encoded String
+    #
+    # Returns UTF 8/16/32 encoded String without BOM or the original String if
+    # no BOM was present.
     def decode_unicode_bom(str)
       BOM.each do |encoding, bytes|
         if str.bytes[0, bytes.size] == bytes
