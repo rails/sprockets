3f5f38ce52a3db8e87cf60fc9685a57fc8cafdb9
Joshua Peek <josh@joshpeek.com>
2014-09-07 22:30:58 -0700

    Revert "Backport matches_filter"
    This reverts commit 40f2810608500e660b235051c1b76a5a6dc6df16.

--- lib/sprockets/base.rb
+++ lib/sprockets/base.rb
@@ -3,7 +3,6 @@ require 'sprockets/bower'
 require 'sprockets/errors'
 require 'sprockets/resolve'
 require 'sprockets/server'
-require 'sprockets/legacy'
 
 module Sprockets
   # `Base` class for `Environment` and `Cached`.
@@ -13,7 +12,6 @@ module Sprockets
     include Server
     include Resolve
     include Bower
-    include Legacy
 
     # Get persistent cache store
     attr_reader :cache
