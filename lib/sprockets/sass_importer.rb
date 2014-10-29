f6501336d320315e44392e657027d5d4836d6064
Joshua Peek <josh@joshpeek.com>
2014-10-29 10:53:16 -0700

    Revert "Avoid exposing a context reference to SassImporter"
    This reverts commit 4dd869b38ab66744a979c9548e54e2900794791b.

--- lib/sprockets/sass_importer.rb
+++ lib/sprockets/sass_importer.rb
@@ -1,20 +1,19 @@
 require 'sass'
 
 module Sprockets
-  # This custom importer that tracks all imported filenames during
-  # compile.
+  # This custom importer adds sprockets dependency tracking on to Sass
+  # `@import` statements. This makes the Sprockets and Sass caching
+  # systems work together.
   class SassImporter < Sass::Importers::Filesystem
-    attr_reader :imported_filenames
-
-    def initialize(*args)
-      @imported_filenames = []
-      super
+    def initialize(context, root)
+      @context = context
+      super root.to_s
     end
 
     def find_relative(*args)
       engine = super
       if engine && (filename = engine.options[:filename])
-        @imported_filenames << filename
+        @context.depend_on(filename)
       end
       engine
     end
@@ -22,7 +21,7 @@ module Sprockets
     def find(*args)
       engine = super
       if engine && (filename = engine.options[:filename])
-        @imported_filenames << filename
+        @context.depend_on(filename)
       end
       engine
     end
--- lib/sprockets/sass_template.rb
+++ lib/sprockets/sass_template.rb
@@ -42,21 +42,15 @@ module Sprockets
         :line => line,
         :syntax => syntax,
         :cache_store => cache_store,
-        :importer => SassImporter.new(context.pathname.to_s),
-        :load_paths => context.environment.paths.map { |path| SassImporter.new(path.to_s) },
+        :importer => SassImporter.new(context, context.pathname),
+        :load_paths => context.environment.paths.map { |path| SassImporter.new(context, path) },
         :sprockets => {
           :context => context,
           :environment => context.environment
         }
       }
 
-      result = ::Sass::Engine.new(data, options).render
-
-      # Track all imported files
-      filenames = ([options[:importer].imported_filenames] + options[:load_paths].map(&:imported_filenames)).flatten.uniq
-      filenames.each { |filename| context.depend_on(filename) }
-
-      result
+      ::Sass::Engine.new(data, options).render
     rescue ::Sass::SyntaxError => e
       # Annotates exception message with parse line number
       context.__LINE__ = e.sass_backtrace.first[:line]
