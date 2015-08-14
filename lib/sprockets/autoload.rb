5ba1899781d390b22f5351aa1b0f9b803102cbce
malida <malida310892@gmail.com>
2015-08-14 13:17:48 -0500

    Add support for Jsminc

--- lib/sprockets/autoload.rb
+++ lib/sprockets/autoload.rb
@@ -5,6 +5,7 @@ module Sprockets
     autoload :CoffeeScript, 'sprockets/autoload/coffee_script'
     autoload :Eco, 'sprockets/autoload/eco'
     autoload :EJS, 'sprockets/autoload/ejs'
+    autoload :JSMinC, 'sprockets/autoload/jsminc'    
     autoload :Sass, 'sprockets/autoload/sass'
     autoload :Uglifier, 'sprockets/autoload/uglifier'
     autoload :YUI, 'sprockets/autoload/yui'
