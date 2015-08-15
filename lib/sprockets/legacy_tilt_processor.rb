5e5cdc1878103c7b3f2ff47352b301f802364cac
Rafael Mendonça França <rafaelmfranca@gmail.com>
2015-08-15 02:51:33 -0300

    Always pass locals as empty hash
    This way we will keep backward compatible to sprockets 2.x version.

--- lib/sprockets/legacy_tilt_processor.rb
+++ lib/sprockets/legacy_tilt_processor.rb
@@ -22,7 +22,7 @@ module Sprockets
       data     = input[:data]
       context  = input[:environment].context_class.new(input)
 
-      data = @klass.new(filename) { data }.render(context)
+      data = @klass.new(filename) { data }.render(context, {})
       context.metadata.merge(data: data.to_str)
     end
   end
