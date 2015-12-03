2b55eb792623399d757c7712ac468947c216fa15
akko <colorful.circle.42@gmail.com>
2015-12-03 23:49:47 +0900

    Remove message not to disclose full path in the browser.

--- lib/sprockets/server.rb
+++ lib/sprockets/server.rb
@@ -139,9 +139,9 @@ module Sprockets
       # Returns a JavaScript response that re-throws a Ruby exception
       # in the browser
       def javascript_exception_response(exception)
-        err  = "#{exception.class.name}: #{exception.message}\n  (in #{exception.backtrace[0]})"
+        err  = "#{exception.class.name}"
         body = "throw Error(#{err.inspect})"
-        [ 200, { "Content-Type" => "application/javascript", "Content-Length" => body.bytesize.to_s }, [ body ] ]
+        [ 500, { "Content-Type" => "application/javascript", "Content-Length" => body.bytesize.to_s }, [ body ] ]
       end
 
       # Returns a CSS response that hides all elements on the page and
@@ -194,7 +194,7 @@ module Sprockets
           }
         CSS
 
-        [ 200, { "Content-Type" => "text/css; charset=utf-8", "Content-Length" => body.bytesize.to_s }, [ body ] ]
+        [ 500, { "Content-Type" => "text/css;charset=utf-8", "Content-Length" => body.bytesize.to_s }, [ body ] ]
       end
 
       # Escape special characters for use inside a CSS content("...") string
--- test/test_server.rb
+++ test/test_server.rb
@@ -234,13 +234,13 @@ class TestServer < Sprockets::TestCase
 
   test "re-throw JS exceptions in the browser" do
     get "/assets/missing_require.js"
-    assert_equal 200, last_response.status
-    assert_equal "throw Error(\"Sprockets::FileNotFound: couldn't find file 'notfound' with type 'application/javascript'\\n  (in #{fixture_path("server/vendor/javascripts/missing_require.js")}:1)\")", last_response.body
+    assert_equal 500, last_response.status
+    assert_equal "throw Error(\"Sprockets::FileNotFound\")", last_response.body
   end
 
   test "display CSS exceptions in the browser" do
     get "/assets/missing_require.css"
-    assert_equal 200, last_response.status
+    assert_equal 500, last_response.status
     assert_match %r{content: ".*?Sprockets::FileNotFound}, last_response.body
   end
 
