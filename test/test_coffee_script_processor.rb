6612068027a843421e797dca9c1310c53f7ea303
Rafael Mendonça França <rafaelmfranca@gmail.com>
2015-12-03 19:33:19 -0200

    Fix test with newest CoffeeScript version

--- test/test_coffee_script_processor.rb
+++ test/test_coffee_script_processor.rb
@@ -13,7 +13,7 @@ class TestCoffeeScriptProcessor < MiniTest::Test
     }
     result = Sprockets::CoffeeScriptProcessor.call(input)
     assert result[:data].match(/var square/)
-    assert_equal 19, result[:map].size
+    assert_equal 13, result[:map].size
     assert_equal [], result[:map].map { |m| m[:source] }.uniq.compact
   end
 
