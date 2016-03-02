46d850951b55d7e0bb9ce9f7e031435ef3789fe6
Thomas Walpole <twalpole@gmail.com>
2016-03-02 13:44:55 -0800

    sassc requires the template to be modifiable

--- lib/sprockets/sassc_compressor.rb
+++ lib/sprockets/sassc_compressor.rb
@@ -15,7 +15,9 @@ module Sprockets
     end
 
     def call(input)
-      data = Autoload::SassC::Engine.new(input[:data], @options.merge(filename: 'filename')).render
+      # SassC requires the template to be modifiable
+      input_data = input[:data].frozen? ? input[:data].dup : input[:data]
+      data = Autoload::SassC::Engine.new(input_data, @options.merge(filename: 'filename')).render
 
       match_data = data.match(/(.*)\n\/\*# sourceMappingURL=data:application\/json;base64,(.+) \*\//m)
       css, map = match_data[1], Base64.decode64(match_data[2])
