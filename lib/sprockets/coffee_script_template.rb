0db3ff6d23940553d7b30db90dfaea04a1fefd9d
schneems <richard.schneeman@gmail.com>
2016-06-21 10:26:55 -0500

    Update deprecation with missing interfaces
    Use `Deprecation` since it is private and we cannot call `Sprockets::Deprecation`.

--- lib/sprockets/coffee_script_template.rb
+++ lib/sprockets/coffee_script_template.rb
@@ -10,7 +10,7 @@ module Sprockets
     end
 
     def self.call(*args)
-      Sprockets::Deprecation.new.warn "CoffeeScriptTemplate is deprecated please use CoffeeScriptProcessor instead"
+      Deprecation.new.warn "CoffeeScriptTemplate is deprecated please use CoffeeScriptProcessor instead"
       CoffeeScriptProcessor.call(*args)
     end
   end
