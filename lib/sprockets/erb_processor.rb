cbcd098058d8fe0c760f081c1d7aefea82b479f4
Matthew Draper <matthew@trebex.net>
2016-10-12 08:52:24 +1030

    Unnest ERBProcessor to keep Sprockets out the constant scope chain

--- lib/sprockets/erb_processor.rb
+++ lib/sprockets/erb_processor.rb
@@ -1,34 +1,32 @@
 # frozen_string_literal: true
 require 'erb'
 
-module Sprockets
-  class ERBProcessor
-    # Public: Return singleton instance with default options.
-    #
-    # Returns ERBProcessor object.
-    def self.instance
-      @instance ||= new
-    end
+class Sprockets::ERBProcessor
+  # Public: Return singleton instance with default options.
+  #
+  # Returns ERBProcessor object.
+  def self.instance
+    @instance ||= new
+  end
 
-    def self.call(input)
-      instance.call(input)
-    end
+  def self.call(input)
+    instance.call(input)
+  end
 
-    def initialize(&block)
-      @block = block
-    end
+  def initialize(&block)
+    @block = block
+  end
 
-    def call(input)
-      engine = ::ERB.new(input[:data], nil, '<>')
-      engine.filename = input[:filename]
+  def call(input)
+    engine = ::ERB.new(input[:data], nil, '<>')
+    engine.filename = input[:filename]
 
-      context = input[:environment].context_class.new(input)
-      klass = (class << context; self; end)
-      klass.const_set(:ENV, context.env_proxy)
-      klass.class_eval(&@block) if @block
+    context = input[:environment].context_class.new(input)
+    klass = (class << context; self; end)
+    klass.const_set(:ENV, context.env_proxy)
+    klass.class_eval(&@block) if @block
 
-      data = engine.result(context.instance_eval('binding'))
-      context.metadata.merge(data: data)
-    end
+    data = engine.result(context.instance_eval('binding'))
+    context.metadata.merge(data: data)
   end
 end
