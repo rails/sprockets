64fadf90882761ed5baac3546c4435c600c50ae5
Emil Sågfors <emil.sagfors@iki.fi>
2016-10-15 00:52:20 +0300

    Make environment version affect asset digest

--- test/test_environment.rb
+++ test/test_environment.rb
@@ -671,10 +671,10 @@ class TestEnvironment < Sprockets::TestCase
     assert_equal 2, asset.metadata[:selector_count]
   end
 
-  test "changing version doesn't affect the assets digest" do
+  test "changing version changes the digest of the asset" do
     old_asset_digest = @env["gallery.js"].hexdigest
     @env.version = 'v2'
-    assert old_asset_digest == @env["gallery.js"].hexdigest
+    assert old_asset_digest != @env["gallery.js"].hexdigest
   end
 
   test "bundled asset is stale if its mtime is updated or deleted" do
