c4a491884e1d24e0cfda37a91a3e70c1ee944f24
Jacob Bednarz <jacob.bednarz@gmail.com>
2017-05-16 15:55:59 +1000

    Delete version change test
    I don't think this was testing what it thought it was.

--- test/test_environment.rb
+++ test/test_environment.rb
@@ -721,12 +721,6 @@ class TestEnvironment < Sprockets::TestCase
     assert_equal 2, asset.metadata[:selector_count]
   end
 
-  test "changing version changes the digest of the asset" do
-    old_asset_digest = @env["gallery.js"].hexdigest
-    @env.version = 'v2'
-    assert old_asset_digest != @env["gallery.js"].hexdigest
-  end
-
   test "bundled asset is stale if its mtime is updated or deleted" do
     filename = File.join(fixture_path("default"), "tmp.js")
 
