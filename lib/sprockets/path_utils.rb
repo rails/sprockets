078a4480618921aba48284c3219e83e98e2da09d
Jan Biedermann <jan@kursator.de>
2017-11-25 05:30:34 +0100

    provide meta data methods and use them internally

--- lib/sprockets/path_utils.rb
+++ lib/sprockets/path_utils.rb
@@ -13,7 +13,7 @@ module Sprockets
     # Returns nil if the file does not exist.
     def stat(path)
       if File.exist?(path)
-        File.stat(path.to_s)
+        File.stat(path)
       else
         nil
       end
@@ -25,11 +25,8 @@ module Sprockets
     #
     # Returns true path exists and is a file.
     def file?(path)
-      if stat = self.stat(path)
-        stat.file?
-      else
-        false
-      end
+      mdata = self.meta_data(path)
+      mdata[:file] if mdata
     end
 
     # Public: Like `File.directory?`.
@@ -38,11 +35,8 @@ module Sprockets
     #
     # Returns true path exists and is a directory.
     def directory?(path)
-      if stat = self.stat(path)
-        stat.directory?
-      else
-        false
-      end
+      mdata = self.meta_data(path)
+      mdata[:directory] if mdata
     end
 
     # Public: A version of `Dir.entries` that filters out `.` files and `~`
@@ -52,14 +46,9 @@ module Sprockets
     #
     # Returns an empty `Array` if the directory does not exist.
     def entries(path)
-      if File.directory?(path)
-        entries = Dir.entries(path, :encoding => Encoding.default_internal)
-        entries.reject! { |entry|
-          entry.start_with?(".".freeze) ||
-            (entry.start_with?("#".freeze) && entry.end_with?("#".freeze)) ||
-            entry.end_with?("~".freeze)
-        }
-        entries.sort!
+      mdata = self.meta_data(path)
+      if mdata && mdata[:directory]
+        mdata[:directory_entries]
       else
         []
       end
@@ -80,7 +69,7 @@ module Sprockets
       end
     else
       def absolute_path?(path)
-        path[0] == File::SEPARATOR
+        path.start_with?(File::SEPARATOR)
       end
     end
 
@@ -282,6 +271,92 @@ module Sprockets
       File.rename(tmpname, filename)
     ensure
       File.delete(tmpname) if File.exist?(tmpname)
+    end
+
+    # Private: Get meta data path from cache
+    # if meta data is not in cache or is obsolete it gets updatet.
+    # this is meant to be the only access path for file or directory meta data,
+    # because accesses are cached and this improves performance multiple times
+    # for many and large asset paths.
+    #
+    # updates meta_data if determined necessary
+    #
+    # returns: meta_data hash or nil
+    #
+    # meta_data: see below in update_meta_data
+
+    def meta_data(path)
+      # file-meta-data:version:path, update version if meta_data hash contents are changed to invalidate existing cached data
+      key = "file-meta-data:1:#{path}"
+      meta_data = cache.get(key)
+      if meta_data && !meta_data[:file_exist] && meta_data[:last_visited_i] < Sprockets::Environment.start_time_i
+        meta_data = update_meta_data(path, key, meta_data)
+      elsif meta_data && meta_data[:static] && meta_data[:last_visited_i] < Sprockets::Environment.start_time_i
+        meta_data = update_meta_data(path, key, meta_data)
+      elsif meta_data && !meta_data[:static]
+        meta_data = update_meta_data(path, key, meta_data)
+      elsif meta_data.nil?
+        meta_data = update_meta_data(path, key, meta_data)
+      end
+      meta_data
+    end
+
+    private
+
+    def update_meta_data(path, key, meta_data)
+      fstat = if File.exist?(path)
+                File.stat(path)
+              else
+                nil
+              end
+      if fstat && (meta_data.nil? || (meta_data && meta_data[:mtime] != fstat.mtime) || (meta_data && meta_data[:static]))
+        dentries = nil
+        if fstat.directory?
+          dentries = Dir.entries(path, :encoding => Encoding.default_internal)
+          dentries.reject! { |entry|
+            entry.start_with?(".".freeze) ||
+            entry.start_with?("..".freeze) ||
+            (entry.start_with?("#".freeze) && entry.end_with?("#".freeze)) ||
+            entry.end_with?("~".freeze)
+          }
+          dentries.sort!
+        end
+        meta_data = {
+          directory: fstat.directory?,
+          directory_entries: dentries,
+          file: fstat.file?,
+          file_digest_uri: URIUtils.build_file_digest_uri(path),
+          mtime: fstat.mtime,
+          size: fstat.size,
+          static: static_path?(path),
+          stat_digest: self.stat_digest_dir(path, fstat, dentries),
+          last_visited_i: Time.now.to_i,
+          exist: true
+        }
+        cache.set(key, meta_data)
+      elsif fstat.nil? && !path.start_with?(root)
+        meta_data = {
+          directory: false,
+          directory_entries: nil,
+          file: false,
+          file_digest_uri: nil,
+          mtime: Time.now,
+          size: nil,
+          static: true,
+          stat_digest: nil,
+          last_visited_i: Time.now.to_i,
+          exist: false
+        }
+        cache.set(key, meta_data)
+      end
+      meta_data
+    end
+
+    def static_path?(path)
+      self.check_modified_paths.each do |mod_path|
+        return false if path.start_with?(mod_path)
+      end
+      true
     end
   end
 end
