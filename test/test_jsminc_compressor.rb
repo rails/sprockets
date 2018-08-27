7de2c8ed7aabdae128da2aa8c74da84db16ea9b3
pavel <pavel.rosicky@easy.cz>
2018-08-27 23:26:06 +0200

    update ruby versions

--- test/test_jsminc_compressor.rb
+++ test/test_jsminc_compressor.rb
@@ -1,22 +1,24 @@
 # frozen_string_literal: true
 require 'minitest/autorun'
 require 'sprockets/cache'
-require 'sprockets/jsminc_compressor'
+unless RUBY_PLATFORM.include?('java')
+  require 'sprockets/jsminc_compressor'
 
-class TestJSMincCompressor < MiniTest::Test
+  class TestJSMincCompressor < MiniTest::Test
 
-  def test_compress_javascript
-    input = {
-      content_type: 'application/javascript',
-      data: "function foo() {\n  return true;\n}",
-      cache: Sprockets::Cache.new
-    }
-    output = "function foo(){return true;}"
+    def test_compress_javascript
+      input = {
+        content_type: 'application/javascript',
+        data: "function foo() {\n  return true;\n}",
+        cache: Sprockets::Cache.new
+      }
+      output = "function foo(){return true;}"
 
-    assert_equal output, Sprockets::JSMincCompressor.call(input)
-  end
+      assert_equal output, Sprockets::JSMincCompressor.call(input)
+    end
 
-  def test_cache_key
-    assert Sprockets::JSMincCompressor.cache_key
+    def test_cache_key
+      assert Sprockets::JSMincCompressor.cache_key
+    end
   end
 end
